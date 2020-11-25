eaf6a0bbba317eb1b722387655411a346287b741
Greg Wilson <grwilson@google.com>
2020-11-25 14:57:35 -0800

    So close

--- image-generator/index.js
+++ image-generator/index.js
@@ -1,10 +1,19 @@
 exports.createImage = (req, res) => {
 
-	var fen = req.query.fen;
+	var fen = null;
+	try {
+		fen = decodeURI(req.url.split('.')[0]).substr(1);	
+	} catch(e) {
+		console.log("Couldn't parse URL to get FEN");
+		fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	}
+	
+	console.log(fen);
 	var ChessImageGenerator = require('chess-image-generator');
 	var imageGenerator = new ChessImageGenerator();
 	if(fen == null) fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-	imageGenerator.loadFEN(fen);
+
+	imageGenerator.loadFEN(fen);	
 
 	createImage(imageGenerator).then((i) => {
 		res.setHeader('Content-Type', 'image/png');
