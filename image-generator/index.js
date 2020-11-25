6ad8a37440a340124a7aead0a475c6d6869462cb
Greg <greg@Gregs-MBP.lan>
2020-11-24 23:24:02 -0800

    Added image generator

---
+++ image-generator/index.js
@@ -0,0 +1,20 @@
+exports.helloWorld = (req, res) => {
+
+	var fen = req.query.fen;
+	var ChessImageGenerator = require('chess-image-generator');
+	var imageGenerator = new ChessImageGenerator();
+	if(fen == null) fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	imageGenerator.loadFEN(fen);
+
+	createImage(imageGenerator).then((i) => {
+		res.setHeader('Content-Type', 'image/png');
+		res.end(i);
+	})
+};
+
+
+async function createImage(img) {
+	const i = img.generateBuffer();
+	return i;
+}
+
